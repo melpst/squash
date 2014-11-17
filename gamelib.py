2fd831b05bde2dcacfe66fe8e2d240547492448f
Jitlada Supreeyaporn <melancholic.pastel.cnrl@gmail.com>
2014-11-17 10:53:22 +0700

    First commit

---
+++ gamelib.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+
+class SimpleGame(object):
+    
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def init(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.locals import *
+from gamelib import SimpleGame
+
+class SquashGame(SimpleGame):
+    pass
+
+def main():
+    game = SquashGame()
+    game.run()
+
+if __name__ == '__main__':
+    main()
