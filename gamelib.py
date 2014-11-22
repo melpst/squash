e9f26f6565212e99beb425e74f736a79232d5c2c
Jitlada Supreeyaporn <melancholic.pastel.cnrl@gmail.com>
2014-11-23 00:19:19 +0700

    notify pressed/unpressed keyboard

--- gamelib.py
+++ gamelib.py
@@ -3,14 +3,39 @@ from pygame.locals import *
 
 class SimpleGame(object):
     
-    def __init__(self, title, window_size=(640, 480), fps=60):
+    def __init__(self,
+                title,
+                window_size=(640, 480),
+                fps=60):
         self.title = title
         self.window_size = window_size
         self.fps = fps
+        self.is_terminated = False
         
+    def terminate(self):
+        self.is_terminated = True
+
+    def __handle_events(self):
+        for event in pygame.event.get():
+            key = pygame.key.get_pressed()
+            if event.type == QUIT:
+                self.terminate()
+            elif event.type == KEYDOWN:
+                self.on_key_down(key)
+            elif event.type == KEYUP:
+                self.on_key_up(key)
+
+
+    def on_key_up(self, key):
+        print "up"
+
+    def on_key_down(self, key):
+        print "down"
+    
     def run(self):
         self.init()
-        while True:
+        while not self.is_terminated:
+            self.__handle_events()
             self.update()
             self.render()
             self.clock.tick(self.fps)
