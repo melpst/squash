724aa7b410e552a8761df71d22fadb7da966b3b2
Jitlada Supreeyaporn <melancholic.pastel.cnrl@gmail.com>
2014-11-22 23:10:14 +0700

    using "super" in init (main.py) and add info in game_init, init (gamelib)

--- gamelib.py
+++ gamelib.py
@@ -3,17 +3,27 @@ from pygame.locals import *
 
 class SimpleGame(object):
     
-    def __init__(self):
-        pass
-
+    def __init__(self, title, window_size=(640, 480), fps=60):
+        self.title = title
+        self.window_size = window_size
+        self.fps = fps
+        
     def run(self):
-        pass
+        self.init()
 
     def init(self):
-        pass
+        self.game_init()
 
     def update(self):
         pass
 
     def render(self):
         pass
+
+    def game_init(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.surface = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption(self.title)
+        self.font = pygame.font.SysFont("monospace", 20)
+
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from gamelib import SimpleGame
 class SquashGame(SimpleGame):
     pass
 
+def init(self):
+    super(SquashGame, self).game_init()
+
 def main():
-    game = SquashGame()
+    game = SquashGame("Squash Game")
     game.run()
 
 if __name__ == '__main__':
